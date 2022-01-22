0e7d4da805b23f678a0cafaf1a93873affb0ec8c
José Maria Recolons Lopez-Pinto <peperecolons0@gmail.com>
2022-01-23 00:17:14 +0100

    album controler created

---
+++ webAPIg3/Controllers/AlbumController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webAPIg3.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //hello world
+
+        //return all albums from db
+
+        //return albums from specific year from db
+    }
+}
